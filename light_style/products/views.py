8b89307301ff4d801c55bb3eb54c7af082f01148
Yulia1999 <syulianetttogggg@gmail.com>
2020-06-03 15:49:06 +0300

    Add categories to site and search

--- light_style/products/views.py
+++ light_style/products/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_GET
+
 from products.models import Product, Category
 
-
-def catalog(request):
-    products = Product.objects.all()
-    return render(request, 'products.html', context={'products': products})
+@require_GET
+def catalog(request, category_slug=None):
+    query = request.GET.get('q', '')
+    products = Product.objects.filter(
+        Q(name__contains=query) | Q(description__contains=query) | Q(category__name__contains=query)
+    )
+    category = None
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'products.html', context={'category': category,
+                   'categories': categories, 'products': products})
 
 
 def retrieve(request, id=None):
